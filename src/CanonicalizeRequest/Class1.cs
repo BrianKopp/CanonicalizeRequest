06a7aae1b7c005add5c4799dec45241bf4d5622c
briankopp <briankopp.usa@gmail.com>
2019-10-06 20:25:08 -0600

    Initial code

---
+++ src/CanonicalizeRequest/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace CanonicalizeRequest
+{
+    public static class RequestCanonicalizer
+    {
+        public static string CanonicalRepresentation(this HttpRequest req)
+        {
+            var representation = "";
+            representation += req.Method.ToUpper() + "\n";
+            representation += req.Path + "\n";
+        }
+    }
+}
